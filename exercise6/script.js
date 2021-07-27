e6ac54f74d19f8e85751192f418f41d0729df2e9
emrizki <emrizkidev@gmail.com>
2021-07-27 14:40:30 +0700

    exercise6

---
+++ exercise6/script.js
@@ -0,0 +1,7 @@
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(array.shift());
+console.log(array.sort());
+console.log(array.push("Kiwi"));
+console.log(array.splice(0, 1));
+console.log(array.reverse());
